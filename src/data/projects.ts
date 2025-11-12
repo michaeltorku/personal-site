27a9a408e8b30b6588f3f071560519c42b8d6ebc
Michael Torku <michaeltorku5@gmail.com>
2025-11-11 23:08:46 -0500

    added projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,9 +4,21 @@ import { Project } from "../components/ProjectList";
 export const Projects: Project [] = [
     {
         id: "1",
-        title: "i no fit",
-        date: "May 15, 2023",
-        excerpt: "A building ya hearrrdd"
+        title: "Multithreaded File Uploader",
+        date: "Nov 1, 2025",
+        excerpt: "A locally hosted file uploader that allows for multi-threaded uploads. Uses apache tomcat as the web server. [Java]"
+    },
+    {
+        id: "2",
+        title: "Devlinkup",
+        date: "Oct 14, 2025",
+        excerpt: "Web app for developers to connect with each other and collaborate on projects. [TypeScript]"
+    },
+    {
+        id: "3",
+        title: "AfroBiz ",
+        date: "Nov 11, 2025",
+        excerpt: "Buying and selling platform for African businesses. [TypeScript]"
     }
 ]
 
