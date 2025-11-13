b7c81f334e5f02532b8805732b1cad440aa87cbe
Michael Torku <michaeltorku5@gmail.com>
2025-11-13 14:03:26 -0500

    hide post 0

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -9,38 +9,38 @@ export interface BlogPost {
   readTime: number;
 }
 export const blogPosts: BlogPost[] = [
-  {
-    id: '2',
-    title: 'Waking Up',
-    date: 'January 15, 2025',
-    author: 'Michael Torku',
-    excerpt: 'A reflection on post-graduation life, the comfort of routine, and the journey toward intentional growth through software engineering and communication.',
-    content: `# Waking Up
+  // {
+  //   id: '2',
+  //   title: 'Waking Up',
+  //   date: 'January 15, 2025',
+  //   author: 'Michael Torku',
+  //   excerpt: 'A reflection on post-graduation life, the comfort of routine, and the journey toward intentional growth through software engineering and communication.',
+  //   content: `# Waking Up
   
-  I graduated college in 2024 May with several others. I had and still have an excitement about affecting the world, the global community and my wallet in a meaningful way. The first thing on my todo list post college was a month summer program called GhanaCodes.
+  // I graduated college in 2024 May with several others. I had and still have an excitement about affecting the world, the global community and my wallet in a meaningful way. The first thing on my todo list post college was a month summer program called GhanaCodes.
   
-  Disclaimer: My thoughts go in all directions all at once, so sometimes it leads to a lot of good-inspired yet incomplete - and occasionally unclear - thoughts. I'm trying to fix that. For this piece, being the opener, I'm putting my mind to paper. I intend to plan future pieces and have them be more organized. All that means is don't expect the rest of this to go in a straight line.
+  // Disclaimer: My thoughts go in all directions all at once, so sometimes it leads to a lot of good-inspired yet incomplete - and occasionally unclear - thoughts. I'm trying to fix that. For this piece, being the opener, I'm putting my mind to paper. I intend to plan future pieces and have them be more organized. All that means is don't expect the rest of this to go in a straight line.
   
-  At the risk of sounding arrogant, I have a boisterous confidence in my ability to do most things. So part of graduating college meant I hadn't the slightest notion/plan of how I was going to affect the world et al. It was just going to work. I had a job offer in hand, decent to good software engineering skill depending who you ask and some semblance of grit if you will. 
+  // At the risk of sounding arrogant, I have a boisterous confidence in my ability to do most things. So part of graduating college meant I hadn't the slightest notion/plan of how I was going to affect the world et al. It was just going to work. I had a job offer in hand, decent to good software engineering skill depending who you ask and some semblance of grit if you will. 
   
-  In my mind, I was going to ace my job, get promoted, leave, start a company or two, get rich, and once I was rich… well I hadn't thought much past that but let's leave that up to my moral compass et al. 
+  // In my mind, I was going to ace my job, get promoted, leave, start a company or two, get rich, and once I was rich… well I hadn't thought much past that but let's leave that up to my moral compass et al. 
   
-  I started my job.
+  // I started my job.
   
-  It was cool and fun for some weeks, hard in others and not so fun from time to time. And unlike a class or some other ephemeral activity, I've had to stick with it day in and day out. I'm treated well, compensated well, I have good friends and family and all the good things that make for a comfortable life. Now I'm so comfortable, the days and weeks occasionally blend into one and I lose sight of some of the bigger targets I have. 
+  // It was cool and fun for some weeks, hard in others and not so fun from time to time. And unlike a class or some other ephemeral activity, I've had to stick with it day in and day out. I'm treated well, compensated well, I have good friends and family and all the good things that make for a comfortable life. Now I'm so comfortable, the days and weeks occasionally blend into one and I lose sight of some of the bigger targets I have. 
   
-  In some ways I think I'm wasting away but I'd say sleep-walking might be a better way to put it. I'm walking, moving (proverbially) forward as far as having some impact at work and making connections and memories and happiness. At the same time I'm asleep, blending into the natural order of things and not bending forces of normalcy and symmetry to my efforts - which has always been part of my assessment of self. 
+  // In some ways I think I'm wasting away but I'd say sleep-walking might be a better way to put it. I'm walking, moving (proverbially) forward as far as having some impact at work and making connections and memories and happiness. At the same time I'm asleep, blending into the natural order of things and not bending forces of normalcy and symmetry to my efforts - which has always been part of my assessment of self. 
   
-  More importantly, how did we get here. By here I mean, in front of my laptop on a Saturday afternoon, looking through my window into the hums of Brooklyn and putting my thoughts on a  screen. I've never been the writing type or more shamefully the reading type for that matter. Not because of any stark opinions, I've just found I enjoyed it less than my other interests so I kept those two next in line on most days.
+  // More importantly, how did we get here. By here I mean, in front of my laptop on a Saturday afternoon, looking through my window into the hums of Brooklyn and putting my thoughts on a  screen. I've never been the writing type or more shamefully the reading type for that matter. Not because of any stark opinions, I've just found I enjoyed it less than my other interests so I kept those two next in line on most days.
   
-  I wouldn't say I've awoken from my slumber, but I'm more cognizant of it. The argument could be made that for a person asleep to be waking themselves up, they'd need to be awake, but let's not lose focus in an analogy I didn't give much thought to. I'm taking an essentialist approach to waking myself up. What are the high leverage skills with a straight line to impact that I am, I need to or I can get good at. I've run through a bunch of these, spending good money along the way. Standing in mostly the same spot as where I started. Of course maybe I wasn't consistent enough among other things. 
+  // I wouldn't say I've awoken from my slumber, but I'm more cognizant of it. The argument could be made that for a person asleep to be waking themselves up, they'd need to be awake, but let's not lose focus in an analogy I didn't give much thought to. I'm taking an essentialist approach to waking myself up. What are the high leverage skills with a straight line to impact that I am, I need to or I can get good at. I've run through a bunch of these, spending good money along the way. Standing in mostly the same spot as where I started. Of course maybe I wasn't consistent enough among other things. 
   
-  More importantly I've sat down to think and I've found the skills that I want to focus on and sharpen on a daily basis. Unfortunately these are all the skills that I overlooked in my selection process. Only two of them at that. **Software Engineering** and **Communication**. I'm taking an execution-based approach which is a long-winded way of saying I'm going to be building tons of software and reading, writing, speaking and listening more consciously. The crux of the problem is that I need to do these in a way that creates exposure and gives me leverage. Like any business or enterprise, that means I need to have a great product (my skillset) and great distribution (tons of people know about me). I haven't quite figured out that part yet. But I'm giving it good thought. 
+  // More importantly I've sat down to think and I've found the skills that I want to focus on and sharpen on a daily basis. Unfortunately these are all the skills that I overlooked in my selection process. Only two of them at that. **Software Engineering** and **Communication**. I'm taking an execution-based approach which is a long-winded way of saying I'm going to be building tons of software and reading, writing, speaking and listening more consciously. The crux of the problem is that I need to do these in a way that creates exposure and gives me leverage. Like any business or enterprise, that means I need to have a great product (my skillset) and great distribution (tons of people know about me). I haven't quite figured out that part yet. But I'm giving it good thought. 
   
-  Note to self (and reader): **Don't sleep. Wake Up.**`,
-    tags: ['Personal', 'Reflection', 'Career', 'Growth'],
-    readTime: 8
-  },
+  // Note to self (and reader): **Don't sleep. Wake Up.**`,
+  //   tags: ['Personal', 'Reflection', 'Career', 'Growth'],
+  //   readTime: 8
+  // },
   {
   id: '0',
   title: 'Lorem Ipsum Dolor Sit Amet',
